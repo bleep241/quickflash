35515fca9ec1eda115304f8fd79145cb466fc8b9
Billy Lee <ucanfindbillie@gmail.com>
2024-07-02 23:43:28 -0700

    MVP flashcards with practice mode

--- src/app/practice/Flashcards.tsx
+++ src/app/practice/Flashcards.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { Category, Flashcard } from '@prisma/client';
-import React, { useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { shuffleArray } from '../../../utils/shuffle';
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
 import { Button } from '@/components/ui/button';
-import { ArrowBigRight } from 'lucide-react';
+import { ArrowBigRight, RotateCcw } from 'lucide-react';
 import { useSuspenseQuery } from '@tanstack/react-query';
 
 function Flashcards({ category }: { category: Category }) {
@@ -19,12 +19,24 @@ function Flashcards({ category }: { category: Category }) {
   });
 
   const flashcards = data.flashcards as Flashcard[];
-
   const [step, setStep] = useState(0);
+  const displayedFlashcards = useMemo(() => shuffleArray(flashcards), [flashcards]);
+  const [answerShown, setAnswerShown] = useState(false);
 
-  console.log("shuffled?", flashcards);
+  const handleNextStep = () => {
+    if (step === displayedFlashcards.length - 1) {
+      setAnswerShown(false);
+      setStep(0);
+      return
+    }
+    setAnswerShown(false);
+    setStep((s) => s + 1);
+  };
 
-  const displayedFlashcards = shuffleArray(flashcards);
+  useEffect(() => {
+    setStep(0);
+    setAnswerShown(false);
+  }, [category])
 
   return (
     <Card className='w-full max-w-md'>
@@ -34,20 +46,31 @@ function Flashcards({ category }: { category: Category }) {
         </CardTitle>
       </CardHeader>
       <CardContent>
-        <Accordion type='single' collapsible>
+        <Accordion
+          value={answerShown ? displayedFlashcards[step].slug : undefined}
+          onValueChange={(value) => setAnswerShown(!!value)}
+          type='single' collapsible>
           <AccordionItem value={displayedFlashcards[step].slug}>
             <AccordionTrigger>
               Reveal answer
             </AccordionTrigger>
             <AccordionContent>
-              {displayedFlashcards[step].answer}
+              {answerShown && displayedFlashcards[step].answer}
             </AccordionContent>
           </AccordionItem>
         </Accordion>
       </CardContent>
-      <CardFooter>
-        <Button variant="outline" size="icon">
-          <ArrowBigRight />
+      <CardFooter className='justify-between'>
+        <div>
+          {step + 1} / {displayedFlashcards.length}
+        </div>
+        <Button onClick={handleNextStep} disabled={!answerShown} variant="outline" size="icon">
+          {step === displayedFlashcards.length - 1 ?
+           (
+            <RotateCcw />
+           ) : (
+            <ArrowBigRight />
+          )}
         </Button>
       </CardFooter>
     </Card>
